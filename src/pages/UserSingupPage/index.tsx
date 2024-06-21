c22d19013bacf6981db9b31ce9a1a9771362e393
Guilherme <guilhermekurpe@gmail.com>
2024-06-21 01:25:55 -0300

    validaçao cadastro

--- src/pages/UserSingupPage/index.tsx
+++ src/pages/UserSingupPage/index.tsx
@@ -11,9 +11,19 @@ export function UserSingupPage() {
     telefone: "",
   });
 
+  const [error, setError] = useState({
+    username: "",
+    email: "",
+    password: "",
+    endereco: "",
+    telefone: "",
+  });
+
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setForm((prev) => ({ ...prev, [name]: value }));
+
+    setError((prev) => ({ ...prev, [name]: undefined }));
   };
 
   const onClickSingup = () => {
@@ -32,8 +42,9 @@ export function UserSingupPage() {
         console.log("cadastrado com sucesso" + response.data);
       })
       .catch((error) => {
-        if (error.response) {
-          console.log("errro ao cadastrar" + error.response.data.message);
+        if (error.response && error.response.data.validationErrors) {
+          console.log("errro ao cadastrar " + error.response.data.message);
+          setError(error.response.data.validationErrors);
         }
       });
   };
@@ -51,9 +62,14 @@ export function UserSingupPage() {
                 <form>
                   <div className="form-group">
                     <label htmlFor="nome">Nome Completo</label>
+                    {error.username && (
+                      <div className="invalid-feedback">{error.username}</div>
+                    )}
                     <input
                       type="text"
-                      className="form-control"
+                      className={
+                        "form-control" + (error.username ? "is-invalid" : "")
+                      }
                       id="username"
                       name="username"
                       placeholder="Digite seu nome completo"
@@ -62,9 +78,14 @@ export function UserSingupPage() {
                   </div>
                   <div className="form-group">
                     <label htmlFor="email">Email</label>
+                    {error.email && (
+                      <div className="invalid-feedback">{error.email}</div>
+                    )}
                     <input
                       type="email"
-                      className="form-control"
+                      className={
+                        "form-control" + (error.email ? "is-invalid" : "")
+                      }
                       id="email"
                       name="email"
                       placeholder="Digite seu email"
@@ -73,15 +94,21 @@ export function UserSingupPage() {
                   </div>
                   <div className="form-group">
                     <label htmlFor="senha">Senha</label>
+                    {error.password && (
+                      <div className="invalid-feedback">{error.password}</div>
+                    )}
                     <input
                       type="password"
-                      className="form-control"
+                      className={
+                        "form-control" + (error.password ? "is-invalid" : "")
+                      }
                       id="password"
                       name="password"
                       placeholder="Digite sua senha"
                       onChange={onChange}
                     />
                   </div>
+                  //precisa validar se as senhas sao iguais
                   <div className="form-group">
                     <label htmlFor="confirmar-senha">Confirmar Senha</label>
                     <input
@@ -94,9 +121,14 @@ export function UserSingupPage() {
                   </div>
                   <div className="form-group">
                     <label htmlFor="endereco">Endereço</label>
+                    {error.endereco && (
+                      <div className="invalid-feedback">{error.endereco}</div>
+                    )}
                     <input
                       type="text"
-                      className="form-control"
+                      className={
+                        "form-control" + (error.endereco ? "is-invalid" : "")
+                      }
                       id="endereco"
                       name="endereco"
                       placeholder="Digite seu endereço"
@@ -105,9 +137,14 @@ export function UserSingupPage() {
                   </div>
                   <div className="form-group">
                     <label htmlFor="telefone">Telefone</label>
+                    {error.telefone && (
+                      <div className="invalid-feedback">{error.telefone}</div>
+                    )}
                     <input
                       type="text"
-                      className="form-control"
+                      className={
+                        "form-control" + (error.telefone ? "is-invalid" : "")
+                      }
                       id="telefone"
                       name="telefone"
                       placeholder="Digite seu telefone"
