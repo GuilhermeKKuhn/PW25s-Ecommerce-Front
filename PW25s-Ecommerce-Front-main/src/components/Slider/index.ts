028586bc47b16ede0246b4ff51e57b542f396a7b
Guilherme <guilhermekurpe@gmail.com>
2024-06-26 19:25:53 -0300

    delete

--- PW25s-Ecommerce-Front-main/src/components/Slider/index.ts
+++
@@ -1,8 +0,0 @@
-/*Organizador de exports*/
-import Slider from './Slider';
-
-import {SwiperSlide, SwiperProps as SliderProps} from 'swiper/react'
-
-
-export {SwiperSlide as Slide, Slider};
-export type {SliderProps};
