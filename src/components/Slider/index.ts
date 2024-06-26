4acc82b3af623c4dd01a66d53e23d771f3aa2ed9
Lucas <lucasbertella@alunos.utfpr.edu.br>
2024-06-26 19:46:04 -0300

    componentes

---
+++ src/components/Slider/index.ts
@@ -0,0 +1,8 @@
+/*Organizador de exports*/
+import Slider from './Slider';
+
+import {SwiperSlide, SwiperProps as SliderProps} from 'swiper/react'
+
+
+export {SwiperSlide as Slide, Slider};
+export type {SliderProps};
