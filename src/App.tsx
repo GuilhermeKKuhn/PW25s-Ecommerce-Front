f25123ae509047e9480e75f10951185af2614257
Lucas <lucasbertella@alunos.utfpr.edu.br>
2024-06-26 19:56:39 -0300

    alteracoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { Image } from "@chakra-ui/react";
+import  HomePage from "./pages/HomePage/homePage";
+import { Slider, SliderProps, Slide} from './components/Slider'
+
 import "./App.css";
-import { BaseRoutes } from "./routes/BaseRoutes";
+import CardProduto from "./components/Cards/Card";
+import { UserSingupPage } from "./pages/UserSingupPage";
+import { UserLoginPage } from "./pages/UserLoginPage";
 
 function App() {
+
   return (
     <>
-      <BaseRoutes />
+
+      {<HomePage />}
+
+      {<Slider></Slider>}
+
+      <div className="container">
+        <div>
+          {<CardProduto/>}
+        </div>
+        <div>
+          {<CardProduto/>}
+        </div>
+        <div>
+          {<CardProduto/>}
+        </div>
+        <div>
+          {<CardProduto/>}
+        </div>
+        <div>
+          {<CardProduto/>}
+        </div>
+        <div>
+          {<CardProduto/>}
+        </div>
+      </div>
     </>
   );
 }
