bdc2e9397cf6d32a569fff28e8685a37d75754c3
Guilherme <guilhermekurpe@gmail.com>
2024-06-30 19:34:23 -0300

    ajuste filto

--- src/components/ListaProduto/ListaProduto.tsx
+++ src/components/ListaProduto/ListaProduto.tsx
@@ -2,56 +2,36 @@ import { IProduct } from "@/commons/interfaces";
 import { useEffect, useState } from "react";
 import CardProduto from "@/components/CardProduto/CardProduto";
 import produtoService from "@/services/ProdutoService";
-import {
-  Flex,
-  Grid,
-  GridItem,
-  Heading,
-  Box,
-} from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Heading, Box } from "@chakra-ui/react";
 
-const ListaProdutos = () => {
-  const [data, setData] = useState<IProduct[]>([]);
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      const response = await produtoService.findAll();
-      setData(response);
-    } catch (error) {
-      console.error("Erro ao buscar produtos:", error);
-    }
-  };
-
+const ListaProdutos = ({ produtos }: { produtos: IProduct[] }) => {
   return (
     <>
-  <Box bg="white" py={10} >
-      <Heading mt={10} textAlign="center">
-        Produtos
-      </Heading>
-      <Flex justify="center" mt={10}>
-        <Grid
-          templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
-          gap={6}
-          w="100%"
-          maxW="95%"
-          px={4}
-          bg="white"
-          borderRadius="md"
-          p={4}
-        >
-          {data.map((produto: IProduct) => (
-            <GridItem key={produto.id}>
-              <CardProduto produto={produto} />
-            </GridItem>
-          ))}
-        </Grid>
-      </Flex>
-    </Box>
+      <Box bg="white" py={10}>
+        <Heading mt={10} textAlign="center">
+          Produtos
+        </Heading>
+        <Flex justify="center" mt={10}>
+          <Grid
+            templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
+            gap={6}
+            w="100%"
+            maxW="95%"
+            px={4}
+            bg="white"
+            borderRadius="md"
+            p={4}
+          >
+            {produtos.map((produto: IProduct) => (
+              <GridItem key={produto.id}>
+                <CardProduto produto={produto} />
+              </GridItem>
+            ))}
+          </Grid>
+        </Flex>
+      </Box>
     </>
   );
 };
+
 export default ListaProdutos;
--- src/services/ProdutoService.ts
+++ src/services/ProdutoService.ts
@@ -20,9 +20,20 @@ const findbyCategoria = async (id: number): Promise<any> => {
   return response.data;
 };
 
+const findById = async (id: number): Promise<any> => {
+  let response;
+  try {
+    response = await api.get(`/produto/${id}`);
+  } catch (error: any) {
+    response = error.response;
+  }
+  return response.data;
+};
+
 const produtoService = {
   findAll,
   findbyCategoria,
+  findById,
 };
 
 export default produtoService;
